646add3e7a7712d8950c17813a64818d5cf645ab
shevourn peart <shevourn.peart@gmail.com>
2023-09-13 12:28:12 -0600

    attacks

---
+++ varsAndConditionals.js
@@ -0,0 +1,16 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+let jonSnowAttack = 25
+let jamieLannisterAttack = 35 
+
+if(jonSnowAttack > jamieLannisterAttack) {
+    console.log('Jon snow has a better attack than Jamie Lannister')
+  }else if (jamieLannisterAttack > jonSnowAttack) {  
+console.log('jamie lannister has a better attack than john Snow') }
+else {
+    console.log('out fighters are evenlt match')
+}
